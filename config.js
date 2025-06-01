1864dfaa82ba8ceaf8bc4436b1808e5eca20b015
Mic <tt402@outlook.com>
2025-06-01 14:36:50 +0800

    fix: 错误忽略config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+window.Config = {
+  // 显示标题
+  SiteName: 'Origin Technology Status Page',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1475397-f42ea16cd410350aa54ef962'
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Home',
+      url: 'https://origin.pw'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/Origin-Technology'
+    }
+  ],
+};
